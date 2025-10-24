ff0e6f41300a05bb9bd7cc31b4bfcc469efee351
Luis Onofre <dev@onofrefranco.com>
2025-10-23 19:28:12 -0600

    Remove PubSub from message service

--- python/message/message_client.py
+++ python/message/message_client.py
@@ -10,25 +10,9 @@ def run(user: str, message: str):
         stub = message_pb2_grpc.MessagingStub(channel)
         stub.Send(message_pb2.UserMessage(user=user, content=message))
 
-
-def negotiate(user):
-    with grpc.insecure_channel('localhost:50052') as channel:
-        stub = message_pb2_grpc.MessagingStub(channel)
-        response = stub.GetConnectionUrl(empty_pb2.Empty())
-        print("url:", response.url)
-
 if __name__ == '__main__':
     # This script sends a single message.
-    # Or negotiates a url which the client
-    # can use to connect with the WebSocket service.
-    try:
-        user = sys.argv[1]
-        message = sys.argv[2]
-    except IndexError:
-        user = "user"
-        message = "Default message"
+    user = sys.argv[1]
+    message = sys.argv[2]
 
-    if message == "--negotiate":
-        negotiate(user)
-    else:
-        run(user, message)
+    run(user, message)
--- python/message/message_service.py
+++ python/message/message_service.py
@@ -1,49 +1,34 @@
 from concurrent import futures
 import logging
-import os
 from dotenv import load_dotenv
 
 import grpc
 import message_pb2_grpc
 import message_pb2
 from google.protobuf import empty_pb2
-from azure.messaging.webpubsubservice import WebPubSubServiceClient
 
 class MessageService(message_pb2_grpc.MessagingServicer):
-    """A service that handles messages that users send. Its job
-    is to distribuite the message to all users that are subscribed to
-    the chat hub."""
-
-    def __init__(self, pubsub : WebPubSubServiceClient):
-        self.pubsub = pubsub
+    """A service that handles messages that users send."""
 
     def Send(self, request, unused_context):
         """It distributes the message to all users"""
         message = request.content
         user = request.user
         logging.info(f"Message received: {message} from {user}")
-        self.pubsub.send_to_all({
-            "from": user,
-            "content": message,
-        })
 
         # This is the way the service returns nothing,
         # as message.proto describes
         return empty_pb2.Empty()
 
     def GetConnectionUrl(self, request, unused_context):
-        response = self.pubsub.get_client_access_token()
-        url = response["url"]
-        return message_pb2.UrlResponse(url=url)
+        """Deprecated"""
+        return message_pb2.UrlResponse(url="Unsupported")
 
 def serve():
     load_dotenv()
-    connection_string = os.environ.get("CONNECTION_STRING")
-    hub_name = os.environ.get("HUB")
-    pubsub_client = WebPubSubServiceClient.from_connection_string(connection_string, hub=hub_name)
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    message_pb2_grpc.add_MessagingServicer_to_server(MessageService(pubsub_client), server)
+    message_pb2_grpc.add_MessagingServicer_to_server(MessageService(), server)
     server.add_insecure_port('[::]:50052')
     server.start()
     server.wait_for_termination()
